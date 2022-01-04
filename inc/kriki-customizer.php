38be4e77da2fca1a51189e74c48dbc4afbdb6497
ashrafbd1496 <asraf1496@gmail.com>
2022-01-04 20:39:21 +0600

    hero , feature and service section done

--- inc/kriki-customizer.php
+++ inc/kriki-customizer.php
@@ -1,5 +1,7 @@
 <?php 
 
+if ( class_exists( 'Kirki' ) ) {
+	
 Kirki::add_config( 'ashraf_kriki_config', array(
 	'capability'    => 'edit_theme_options',
 	'option_type'   => 'theme_mod',
@@ -124,3 +126,240 @@ Kirki::add_field( 'ashraf_kriki_config', [
 		],
 	],
 ] );
+
+
+//About Us section
+
+Kirki::add_section( 'about_section', array(
+    'title'          => esc_html__( 'About Section', 'ashraf_kirki' ),
+    'panel'          => 'ashraf_kriki_panel',
+    'priority'       => 160,
+) );
+
+//About Title
+
+Kirki::add_field( 'ashraf_kriki_config', [
+	'type'     => 'text',
+	'settings' => 'about_title_setting',
+	'label'    => esc_html__( 'About Title', 'ashraf_kirki' ),
+	'section'  => 'about_section',
+	'default'  => esc_html__( 'We are helping to grow
+your business.', 'ashraf_kirki' ),
+	'priority' => 10,
+	'transport'=>'postMessage',
+	'js_vars'=>array(
+		array(
+			'element'=>'.title-hl',
+			'function'=>'html',
+
+		)
+	),
+] );
+
+//About description
+
+Kirki::add_field( 'ashraf_kriki_config', [
+	'type'     => 'text',
+	'settings' => 'about_description_setting',
+	'label'    => esc_html__( 'About Description', 'ashraf_kirki' ),
+	'section'  => 'about_section',
+	'default'  => esc_html__( 'A digital studio specialising in User Experience & eCommerce, we combine innovation with digital craftsmanship to help brands fulfill their potential.', 'ashraf_kirki' ),
+	'priority' => 10,
+	'transport'=>'postMessage',
+	'js_vars'=>array(
+		array(
+			'element'=>'#feature p',
+			'function'=>'html',
+
+		)
+	),
+] );
+
+//About Button Text
+Kirki::add_field( 'ashraf_kriki_config', [
+	'type'     => 'text',
+	'settings' => 'about_button_text',
+	'label'    => __( 'Abut Button Text', 'ashraf_kirki' ),
+	'section'  => 'about_section',
+	'default'  => 'More About Us',
+	'priority' => 10,
+	'transport'=>'postMessage',
+	'js_vars'=>array(
+		array(
+			'element'=>'.btn-common',
+			'function'=>'html',
+
+		)
+	),
+] );
+
+//About Button URL
+Kirki::add_field( 'ashraf_kriki_config', [
+	'type'     => 'link',
+	'settings' => 'about_button_link',
+	'label'    => __( 'About Button Link', 'ashraf_kirki' ),
+	'section'  => 'about_section',
+	'default'  => 'https://kriki.ashrafbd.com/',
+	'priority' => 10,
+] );
+
+//About section Repeater Items
+
+Kirki::add_field( 'ashraf_kriki_config', [
+	'type'        => 'repeater',
+	'label'       => esc_html__( 'About Items', 'ashraf_kirki' ),
+	'section'     => 'about_section',
+	'priority'    => 10,
+	'row_label' => [
+		'type'  => 'field',
+		'value' => esc_html__( 'Add New Item', 'ashraf_kirki' ),
+		'field' => 'about_item_title',
+	],
+	'button_label' => esc_html__('Add New About Item', 'ashraf_kirki' ),
+	'settings'     => 'about_repeater',
+	'default'      => [
+		[
+            'about_item_icon' => 'lni-microphone',
+			'about_item_title' => __( 'What we do', 'ashraf_kirki' ),
+			'about_item_desc'  => __( 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia con- sequuntur magni dolores ', 'ashraf_kirki' ),
+		],
+	],
+	'fields' => [
+        'about_item_icon' => [
+			'type'        => 'select',
+			'label'       => esc_html__( 'About Item Icon', 'ashraf_kirki' ),
+			'default'     => '',
+            'choices'     => array(
+                'lni-microphone' => __('Icon 1', 'ashraf_kirki'),
+                'lni-users' => __('Icon 2', 'ashraf_kirki'),
+                'lni-medall-alt' => __('Icon 3', 'ashraf_kirki'),
+            )
+		],
+		'about_item_title' => [
+			'type'        => 'text',
+			'label'       => esc_html__( 'About Item Title', 'ashraf_kirki' ),
+			'default'     => '',
+		],
+		'about_item_desc' => [
+			'type'        => 'textarea',
+			'label'       => esc_html__( 'About Item Description', 'ashraf_kirki' ),
+			'default'     => '',
+		],
+	],
+    'choices' => [
+		'limit' => 3
+	],
+] );
+
+
+
+//Services section
+
+Kirki::add_section( 'service_section', array(
+    'title'          => esc_html__( 'Service Section', 'ashraf_kirki' ),
+    'panel'          => 'ashraf_kriki_panel',
+    'priority'       => 160,
+) );
+
+//Service Sedtion Show Hide
+Kirki::add_field( 'ashraf_kriki_config', [
+	'type'        => 'checkbox',
+	'settings'    => 'service_show',
+	'label'       => esc_html__( 'Show Service Section', 'kirki' ),
+	'section'     => 'service_section',
+	'default'     => true,
+] );
+
+//Service Title
+
+Kirki::add_field( 'ashraf_kriki_config', [
+	'type'     => 'text',
+	'settings' => 'service_title',
+	'label'    => esc_html__( 'Service Title', 'ashraf_kirki' ),
+	'section'  => 'service_section',
+	'default'  => esc_html__( 'Our Services', 'ashraf_kirki' ),
+	'priority' => 10,
+	'transport'=>'postMessage',
+	'js_vars'=>array(
+		array(
+			'element'=>'#services h2',
+			'function'=>'html',
+
+		)
+	),
+] );
+
+//Service description
+
+Kirki::add_field( 'ashraf_kriki_config', [
+	'type'     => 'text',
+	'settings' => 'service_description',
+	'label'    => esc_html__( 'Service Description', 'ashraf_kirki' ),
+	'section'  => 'service_section',
+	'default'  => esc_html__( 'A desire to help and empower others between community contributors in technology
+began to grow in 2020.', 'ashraf_kirki' ),
+	'priority' => 10,
+	'transport'=>'postMessage',
+	'js_vars'=>array(
+		array(
+			'element'=>'#services p',
+			'function'=>'html',
+
+		)
+	),
+] );
+
+
+//Service section Repeater Items
+
+
+Kirki::add_field( 'ashraf_kriki_config', [
+	'type'        => 'repeater',
+	'label'       => esc_html__( 'Service Items', 'ashraf_kirki' ),
+	'section'     => 'service_section',
+	'priority'    => 10,
+	'row_label' => [
+		'type'  => 'field',
+		'value' => esc_html__( 'Add New Item', 'ashraf_kirki' ),
+		'field' => 'service_item_title',
+	],
+	'button_label' => esc_html__('Add New About Item', 'ashraf_kirki' ),
+	'settings'     => 'service_repeater',
+	'default'      => [
+		[
+            'service_item_icon' => 'lni-pencil',
+			'service_item_title' => __( 'Content Writing', 'ashraf_kirki' ),
+			'service_item_desc'  => __( 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde perspiciatis dicta labore nulla beatae quaerat quia incidunt laborum aspernatur...', 'ashraf_kirki' ),
+		],
+	],
+	'fields' => [
+        'service_item_icon' => [
+			'type'        => 'select',
+			'label'       => esc_html__( 'Service Item Icon', 'ashraf_kirki' ),
+			'default'     => 'lni-pencil',
+            'choices'     => array(
+                'lni-pencil' => __('Pencil', 'ashraf_kirki'),
+                'lni-briefcase' => __('Briefcase', 'ashraf_kirki'),
+                'lni-cog' => __('Cog', 'ashraf_kirki'),
+                'lni-mobile' => __('Mobile', 'ashraf_kirki'),
+                'lni-layers' => __('Layers', 'ashraf_kirki'),
+                'lni-rocket' => __('Rocket', 'ashraf_kirki'),
+            )
+		],
+		'service_item_title' => [
+			'type'        => 'text',
+			'label'       => esc_html__( 'Service Item Title', 'ashraf_kirki' ),
+			'default'     => '',
+		],
+		'service_item_desc' => [
+			'type'        => 'textarea',
+			'label'       => esc_html__( 'Service Item Description', 'ashraf_kirki' ),
+			'default'     => '',
+		],
+	],
+    'choices' => [
+		'limit' => 6
+	],
+] );
+
+}
